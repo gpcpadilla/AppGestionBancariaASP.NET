efdb6f2c10fe75c2a5c8ecd9a66d2794bddd159c
gpcpadilla <geovanito25@hotmail.com>
2015-02-25 16:01:27 -0500

    Update1

--- AppGestionBancaria/Cheque.cs
+++ AppGestionBancaria/Cheque.cs
@@ -70,5 +70,11 @@ namespace AppGestionBancaria
         }
         #endregion
 
+
+          #region "Metodos"
+        public void BalanceAtual(Saldo); 
+        public void Depositar(BalanceActual + deposito); 
+        public void Retirar(BalanceActual - Retiro);
+        #endregion
     }
 }
