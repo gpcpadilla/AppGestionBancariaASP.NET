af2bad4e0d243658e17b7785d6a912042ae64b18
gpcpadilla <geovanito25@hotmail.com>
2015-02-25 16:08:08 -0500

    nueva

--- AppGestionBancaria/Cuentas.cs
+++ AppGestionBancaria/Cuentas.cs
@@ -109,5 +109,11 @@ namespace AppGestionBancaria
             return this.ToString().GetHashCode();
         }
         #endregion
+
+          #region "Metodos"
+        public void BalanceAtual(Saldo); 
+        public void Depositar(BalanceActual + deposito); 
+        public void Retirar(BalanceActual - Retiro);
+        #endregion
     }
 }
